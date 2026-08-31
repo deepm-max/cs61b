b232318e6c3f924fdbe5574abf0cbed9b4cb0c46
Deep Thakkar <deepm@maxielatech.com>
2026-08-31 12:58:25 +0530

    done with collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,9 +1,11 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Deep Thakkar
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Fixed implementation of nextNumber it takes
+     * @param n and if it's even then it returns n/2 else it returns 3 * n + 1.
+     */
     public static int nextNumber(int n) {
         if (n  % 2 == 0) {
             return n/2;
